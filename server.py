8ab0502ba01a124472a90a9af038ce1bed8268d0
dlalsdn6166 <39112520+dlalsdn6166@users.noreply.github.com>
2022-11-30 17:44:37 +0900

    handle client

--- server.py
+++ server.py
@@ -9,12 +9,16 @@ address = '0.0.0.0'
 port = 9879
 
 def handleClient(client_socket:socket, client_address):
+    print('!--- Request from {} ---!'.format(client_address))
     start = time.time()
-    data = client_socket.recv(65535)
+    data = client_socket.recv(4)
+    img_size = int.from_bytes(data, byteorder = 'big')    
+    print('# image_size: {}'.format(img_size))
+    data = client_socket.recv(img_size)
     result = myutil.encode(myocr.read(data))
     client_socket.sendall(len(result).to_bytes(4,byteorder = 'big'))
     client_socket.sendall(result)
-    print('!--- Detection from {} ---!\n# result: {}\n# time_spent: {}'.format(client_address, result.decode(), time.time()-start))
+    print('# result: {}\n# time_spent: {}'.format(result.decode(), time.time()-start))
 
 def Terminate(signum, frame):
     print('Terminating program...')
